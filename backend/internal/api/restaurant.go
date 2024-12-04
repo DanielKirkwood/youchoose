f71a88fbefcae2ec10ce15afaedfb3175f7173ba
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2024-12-04 22:47:19 +0000

    search for restaurants

--- backend/internal/api/restaurant.go
+++ backend/internal/api/restaurant.go
@@ -25,12 +25,26 @@ type GetNearestRestaurantsOutput struct {
 	Body GetNearestRestaurantsBody
 }
 
+type SearchRestaurantsInput struct {
+	Limit  int32  `query:"limit" required:"false" default:"30" doc:"the maximum number of rows to return" example:"100"`
+	Offset int32  `query:"offset" required:"false" default:"0" doc:"the row to start from" example:"20"`
+	Query  string `query:"query" required:"true" doc:"the search term" example:"Gregg"`
+}
+
+type SearchRestaurantsBody struct {
+	Restaurants []db.SearchRestaurantsRow `json:"restaurants"`
+}
+
+type SearchRestaurantsOutput struct {
+	Body SearchRestaurantsBody
+}
+
 func RegisterRestaurantRoutes(api huma.API, restaurantService *services.RestaurantService) {
 	huma.Register(api, huma.Operation{
 		OperationID: "get-nearest-restaurants",
 		Method:      http.MethodGet,
 		Path:        "/restaurants/nearby",
-		Summary:     "Get the restaurants within the given radius to the given location",
+		Summary:     "Get nearest restaurants",
 	}, func(ctx context.Context, i *GetNearestRestaurantsInput) (*GetNearestRestaurantsOutput, error) {
 		radiusMetres := i.Radius * 1609.34
 
@@ -52,6 +66,29 @@ func RegisterRestaurantRoutes(api huma.API, restaurantService *services.Restaura
 				Restaurants: result,
 			},
 		}, nil
+	})
 
+	huma.Register(api, huma.Operation{
+		OperationID: "search-restaurants",
+		Method:      http.MethodGet,
+		Path:        "/restaurants/search",
+		Summary:     "Search for restaurants",
+	}, func(ctx context.Context, i *SearchRestaurantsInput) (*SearchRestaurantsOutput, error) {
+		params := db.SearchRestaurantsParams{
+			Limit:      i.Limit,
+			Offset:     i.Offset,
+			SearchTerm: i.Query,
+		}
+
+		result, err := restaurantService.Queries.SearchRestaurants(ctx, params)
+		if err != nil {
+			return nil, huma.Error500InternalServerError("unable to find restaurants with that search term", err)
+		}
+
+		return &SearchRestaurantsOutput{
+			Body: SearchRestaurantsBody{
+				Restaurants: result,
+			},
+		}, nil
 	})
 }
